5bb3b741572cf08ffd89d4b7028b282c61b9f178
chris-ortiz <christian.ortiz@adesso.de>
2024-11-06 14:52:44 +0100

    day 8 b

--- src/q8.rs
+++ src/q8.rs
@@ -9,16 +9,30 @@ pub fn q8() {
 
     println!("{:?}", tokens);
 
-    let size = calculate_diff(tokens);
+    let size = calculate_diff(&tokens);
 
-    println!("size {}", size)
+    println!("size {}", size);
+
+    let size = calculate_diff_encoded(tokens);
+
+    println!("encoded size {}", size)
 }
 
-fn calculate_diff(tokens: Vec<Token>) -> usize {
+fn calculate_diff(tokens: &Vec<Token>) -> usize {
     let mut size = 0;
 
     for token in tokens {
         size += token.literal_size - token.in_memory_size;
+    }
+
+    size
+}
+
+fn calculate_diff_encoded(tokens: Vec<Token>) -> usize {
+    let mut size = 0;
+
+    for token in tokens {
+        size += token.new_string_size - token.literal_size;
     }
 
     size
@@ -49,6 +63,49 @@ fn tokenize(input: String) -> Vec<Token> {
 }
 
 fn read_string(chars: &Vec<char>, start_index: usize) -> Token {
+    let (i, escaped_string) = escape(chars, start_index);
+    let encoded_string = encode(chars, start_index);
+
+
+    let in_memory_size = escaped_string.len();
+    let encoded_size = encoded_string.len();
+
+    Token {
+        string_value: escaped_string,
+        string_literal: chars[start_index..i].iter().collect(),
+        in_memory_size,
+        literal_size: i - start_index,
+        abs_index: start_index,
+        new_string: encoded_string,
+        new_string_size: encoded_size,
+    }
+}
+
+fn encode(chars: &Vec<char>, start_index: usize) -> String {
+    let mut i = start_index;
+    let mut str: Vec<char> = Vec::new();
+
+    str.push('"');
+    while i < chars.len() && chars[i] != '\n' {
+        if chars[i] == '"' {
+            str.push('\\');
+            str.push('"');
+        } else if chars[i] == '\\' {
+            str.push('\\');
+            str.push('\\');
+        } else {
+            str.push(chars[i]);
+        }
+
+        i += 1
+    }
+
+    str.push('"');
+
+    str.iter().collect()
+}
+
+fn escape(chars: &Vec<char>, start_index: usize) -> (usize, String) {
     let mut i = start_index;
     let mut str: Vec<char> = Vec::new();
 
@@ -81,26 +138,18 @@ fn read_string(chars: &Vec<char>, start_index: usize) -> Token {
         i += 1;
     }
 
-    let clean_value: String = str.iter().collect();
-    let in_memory_size = clean_value.len();
-
-    Token {
-        string_value: clean_value,
-        string_literal: chars[start_index..i].iter().collect(),
-        in_memory_size,
-        literal_size: i - start_index,
-        abs_index: start_index,
-    }
+    (i, str.iter().collect())
 }
-
 
 #[derive(Debug)]
 struct Token {
+    abs_index: usize,
     string_value: String,
     string_literal: String,
+    new_string: String,
     literal_size: usize,
-    abs_index: usize,
     in_memory_size: usize,
+    new_string_size: usize,
 }
 
 
@@ -163,7 +212,13 @@ mod tests {
     fn test_calculation() {
         let token = tokenize(String::from("\"\"\n\"abc\"\n\"aaa\\\"aaa\"\n\"\\x27\""));
         println!("{:?}", token);
-        let diff = calculate_diff(token);
+        let diff = calculate_diff(&token);
         assert_eq!(12, diff);
+    }
+
+    #[test]
+    fn test_encoding() {
+        let token = tokenize(String::from("\"\""));
+        assert_eq!("\"\\\"\\\"\"", token[0].new_string);
     }
 }
